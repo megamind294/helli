aebadde5e3e7d5a1e286e4f79343275d85f3b183
soumi865 <soumi.k@ahaansoftware.com>
2024-08-02 20:10:14 +0530

    homepagefixed

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-const config = {
-    type: 'carousel',
-    startAt: 0,
-    perView: 4,
-    gap: 32,
-    breakpoints: {
-      1280: {
-        perView: 3,
-      },
-      1024: {
-        perView: 2,
-      },
-      768: {
-        perView: 1,
-      }
-    }
-  }
-  new Glide('.glide', config).mount()
+
+
+
